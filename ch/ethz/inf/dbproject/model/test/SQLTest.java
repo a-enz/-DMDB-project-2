0ef2a8d9933f5ac43fe9bc53f724c69e4372c1c3
Daniel Yu <yu_daniel@me.com>
2014-05-26 13:25:30 +0200

    small changes

--- ch/ethz/inf/dbproject/model/test/SQLTest.java
+++ ch/ethz/inf/dbproject/model/test/SQLTest.java
@@ -34,10 +34,6 @@ public class SQLTest {
 		project = (Project) visit;
 		project.printTree(0);
 		
-		if (project.getSchema() == null){
-			System.out.println("AAAAh...");
-		}
-		
 		for (String s:project.getSchema().getAllNames()){
 			System.out.println("columnName: " + s);
 		}
